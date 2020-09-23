472d1f874dc2b35c06300950d36896dd163df482
jbryan22 <jesse.bryan22@gmail.com>
2020-09-23 00:07:18 -0700

    basic routing structure added with higher-order-comp withRouter

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React from "react";
 import "./App.css";
 import HomePage from "./pages/homepage/homepage.component.jsx";
+import { Switch, Route } from "react-router-dom";
+
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>
+);
 
 function App() {
   return (
     <div>
-      <HomePage></HomePage>
+      <Switch></Switch>
+      <Route exact path="/" component={HomePage}></Route>
+      <Route path="/hats" component={HatsPage}></Route>
     </div>
   );
 }
