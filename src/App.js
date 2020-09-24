0ed1585869437dd5f6aee0ddd594cfdda22011dc
jbryan22 <jesse.bryan22@gmail.com>
2020-09-24 13:22:52 -0700

    shop page with collection preview and individual item components created

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./App.css";
 import HomePage from "./pages/homepage/homepage.component.jsx";
 import { Switch, Route } from "react-router-dom";
+import ShopPage from "./pages/shop/shop.component.jsx";
 
 const HatsPage = () => (
   <div>
@@ -12,9 +13,10 @@ const HatsPage = () => (
 function App() {
   return (
     <div>
-      <Switch></Switch>
-      <Route exact path="/" component={HomePage}></Route>
-      <Route path="/hats" component={HatsPage}></Route>
+      <Switch>
+        <Route exact path="/" component={HomePage}></Route>
+        <Route path="/shop" component={ShopPage}></Route>
+      </Switch>
     </div>
   );
 }
